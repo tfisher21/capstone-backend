0fc4763f347dc263ec75346c683dc004efd6a622
Tyler Fisher <tfisher21@gmail.com>
2018-12-16 14:11:45 -0600

    Add dynamic responses based on availability

--- app/controllers/api/sms_controller.rb
+++ app/controllers/api/sms_controller.rb
@@ -1,29 +1,54 @@
 class Api::SmsController < ApplicationController
+  before_action :authenticate_user, except: [:receive_text]
+
   def send_text
     account_sid = ENV["twilio_account_sid"]
     auth_token = ENV["twilio_auth_token"]
     phone_number = ENV["twilio_phone_number"]
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    message = @client.messages.create(body: "Hello World", from: phone_number, to: "+14406106300")
+    body = "Hey! #{current_user.first_name} #{current_user.last_name}, from the #{current_user.cohort.name} cohort at Actualize, would like to buy you coffee. Can you make it? Reply with Y/N"
+    client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    puts message.sid
-    render json: {msg: "Text Sent"}
+    message = client.messages.create(
+      body: body,
+      from: phone_number,
+      to: "+14406106300"
+    )
+
+    puts message
+    render json: {msg: "Text Sent", twilio: message.sid}
   end
 
   def receive_text
-    message_body = ["Body"]
+    message_body = params["Body"]
     from_number = params["From"]
+
+    puts message_body
 
     account_sid = ENV["twilio_account_sid"]
     auth_token = ENV["twilio_auth_token"]
     phone_number = ENV["twilio_phone_number"]
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
-    sms = @client.messages.create(
-      from: phone_number,
-      to: from_number,
-      body: "Hello there, thanks for texting me. Your number is #{from_number}"
-    )
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    if message_body.upcase === "Y"
+      sms = client.messages.create(
+        from: phone_number,
+        to: from_number,
+        body: "Great! I'll let them know!"
+      )
+    elsif message_body.upcase === "N"
+      sms = client.messages.create(
+        from: phone_number,
+        to: from_number,
+        body: "No Problem, I'll let them know to try another time!"
+      )
+    else
+      sms = client.messages.create(
+        from: phone_number,
+        to: from_number,
+        body: "Sorry, I'm not sure how you responded. Please reply with Y for yes, or N for no"
+      )
+    end
 
     render json: {}
   end
